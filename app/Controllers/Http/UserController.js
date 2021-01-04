92036317dc33a1fda6f3b97419b9cb74d1cf2e70
Douglas Shibata <douglas.shibata11@gmail.com>
2021-01-04 20:22:03 -0300

    Verificando senha, cpf

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -2,13 +2,61 @@
 //Para criar um controller adonis make:controller User --type http
 const User = use("App/Models/User");
 const Database = use('Database');
+//Validador de cpf
+const { cpf } =require('cpf-cnpj-validator'); 
+// Validador de Senha
+var passwordValidator = require('password-validator');
+var schema = new passwordValidator();
+const Hash = use('Hash')
+
+
 class UserController {
-  async store({ request }) {
-    const data = request.only(['cpfUser', 'nome' ,'email', 'password']);
-
-    const user = await User.create(data);
-
-    return user;
+  async store({ request ,response}) {
+    try {
+      // Pega os dados e cadastra
+      const data = request.only(['cpfUser', 'nome' ,'email', 'password']);
+      //verifica se o email já está cadastrado
+      const userEmailExists = await User.findBy('email', data.email)
+      //Verifica se o cpf já está cadastrado
+      const userCpfExists = await User.findBy('cpfUser', data.cpfUser)
+      if(!cpf.isValid(data.cpfUser)){
+        console.log(cpf.generate());
+        return response
+          .status(400)
+          .send({ message: { error: 'CPF inválido' } })
+      }
+      if (userEmailExists) {
+        return response
+          .status(400)
+          .send({ message: { error: 'Email Já Cadastrado' } })
+      }
+      if (userCpfExists) {
+        return response
+          .status(400)
+          .send({ message: { error: 'CPF Já Cadastrado' } })
+      }
+      schema
+      .is().min(8)                                    // Minimum length 8
+      .is().max(100)                                  // Maximum length 100
+      .has().uppercase([1])                           // Must have uppercase letters
+      .has().lowercase()                              // Must have lowercase letters
+      .has().digits(2)                                // Must have at least 2 digits
+      .has().symbols([1])	
+      .has().not().spaces()                           // Should not have spaces
+      .is().not().oneOf(['Passw0rd', 'Password123','12345','senha']); // Blacklist these values
+      if(!schema.validate(data.password)){
+        return response
+        .status(400)
+        .send({ message: { error: 'Senha Fraca, deve ter no mínimo 8 caracteres tem que ter no mínimo 1 letra Maiuscula Dois Digitos Sem espaço' } })
+      }
+      const user = await User.create(data);
+  
+      return user;  
+    } catch (error) {
+      return response
+      .status(error.status)
+      .send(error)
+    }
   };
   async index() {
     const user = User.all()
@@ -30,28 +78,40 @@ class UserController {
     return user
   }
 
-  async update({ request}) {
-    const user = await User.findByOrFail("cpfUser", request.header("cpfUser"));
+  async update({ request,response}) {
+    try {
+      const user = await User.findByOrFail("cpfUser", request.header("cpfUser"));
+      
+      const data = request.only([
+        'email',
+        'password',
+        'nome',
+        'telefone',
+        'conselho',
+        'ufConselho',
+        'registro',
+        'especialidade',
+        'ativo',
+        'ehMedico',
+      ])
+      const passwordCheck = await Hash.verify(data.password, user.password)
+      
+    if (!passwordCheck) {
+      return response
+        .status(400)
+        .send({ message: { error: 'Senha Incorreta' } })
+    }
+  
+      user.merge(data)
+  
+      await user.save()
+     console.log(user);
+      return user
+    } catch (error) {
+      console.log(error);
+    }
 
 
-    const data = request.only([
-      'email',
-      'password',
-      'nome',
-      'telefone',
-      'conselho',
-      'ufConselho',
-      'registro',
-      'especialidade',
-      'ativo',
-      'ehMedico',
-    ])
-
-    user.merge(data)
-
-    await user.save()
-
-    return user
   }
   /* async destroy ({ params, auth, response }) {
     const user = await User.findOrFail(params.id)
